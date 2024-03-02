1a67f51e01d18e9d00b9c226ca0d5e4d86894782
Sulaimon Omotosho <susudef2u@gmail.com>
2024-03-02 09:50:57 +0100

    Added responsiveness to footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -11,29 +11,37 @@ const Footer = () => {
         <div className='menu'>
           <div className='details'>
             <h3>About</h3>
-            <a href='/'>About Me</a>
-            <a href='/'>Services</a>
-            <a href='/'>Careers</a>
+            <div className='detailLinks'>
+              <a href='/'>About Me</a>
+              <a href='/'>Services</a>
+              <a href='/'>Careers</a>
+            </div>
           </div>
           <div className='details'>
             <h3>Company</h3>
-            <a href='/'>My team</a>
-            <a href='/'>Partnership</a>
-            <a href='/'>FAQ</a>
-            <a href='/'>blog</a>
-            <a href='/'>privacy policy</a>
+            <div className='detailLinks'>
+              <a href='/'>My team</a>
+              <a href='/'>Partnership</a>
+              <a href='/'>FAQ</a>
+              <a href='/'>blog</a>
+              <a href='/'>privacy policy</a>
+            </div>
           </div>
           <div className='details'>
             <h3>support</h3>
-            <a href='/'>account</a>
-            <a href='/'>support center</a>
-            <a href='/'>feedback</a>
+            <div className='detailLinks'>
+              <a href='/'>account</a>
+              <a href='/'>support center</a>
+              <a href='/'>feedback</a>
+            </div>
           </div>
           <div className='details'>
             <h3>contact</h3>
-            <a href='/'>+234 903 5198 024</a>
-            <a href='/'>sulaimon.omotosho90@gmail.com</a>
-            <div className='socials'></div>
+            <div className='detailLinks'>
+              <a href='/'>+234 903 5198 024</a>
+              <a href='/'>sulaimon.omotosho90@gmail.com</a>
+              <div className='socials'></div>
+            </div>
           </div>
         </div>
       </div>
