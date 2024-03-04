36a690db2626473d017e2536d053691365711d24
Sulaimon Omotosho <susudef2u@gmail.com>
2024-03-04 21:44:51 +0100

    Done with the portfolio section with responsiveness

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -10,7 +10,9 @@ const Portfolio = () => {
       <div className='wrapper'>
         <div className='pContents'>
           <div className='dream'>
-            <h3>turning web imaginations into realities</h3>
+            <h3>
+              turning web imaginations <br /> into realities
+            </h3>
             <p>
               Transforming design imaginations into digital realities is a
               mastered art. With a keen eye for detail and expertise in frontend
@@ -20,9 +22,11 @@ const Portfolio = () => {
             <button>View Projects</button>
           </div>
           <div className='portraits'>
-            <hr className='line1' />
+            <div className='box1'></div>
+            <img className='portfolioImg2' src='Blog Site 2.png' alt='p1' />
+            <div className='box2'></div>
             <img className='portfolioImg1' src='portfolio 1.jpg' alt='p1' />
-            <hr className='line2' />
+            {/* <img className='portfolioImg3' src='Portfolio 3.png' alt='p1' /> */}
           </div>
         </div>
       </div>
