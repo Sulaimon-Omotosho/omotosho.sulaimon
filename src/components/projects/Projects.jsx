64af4d3647ac7ef71ae872978f25cdc3b17d3251
Sulaimon Omotosho <susudef2u@gmail.com>
2024-03-05 00:40:57 +0100

    Still working on projects secion design

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './projects.scss'
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from 'react-icons/bs'
+
+const images = [
+  { id: '1', img: 'pro 1.jpg' },
+  // { id: '2', img: 'pro 2.jpg' },
+  { id: '3', img: 'pro 3.jpg' },
+  { id: '4', img: 'pro 4.jpg' },
+  { id: '5', img: 'pro 5.jpg' },
+  { id: '6', img: 'pro 6.jpg' },
+]
 
 const Projects = () => {
+  const [slide, setSlide] = useState(0)
+
+  const nextSlide = () => {
+    setSlide(slide === images.length - 1 ? 0 : slide + 1)
+  }
+  const prevSlide = () => {
+    setSlide(slide === 0 ? images.length - 1 : slide - 1)
+  }
+
   return (
     <div className='projects'>
       <div className='wrapper'>
@@ -14,24 +33,35 @@ const Projects = () => {
           <button>View Projects</button>
         </div>
         <div className='pro'>
-          <div className='imageWrapper'>
-            <img src='portfolio 1.jpg' alt='' />
-          </div>
-          <div className='imageWrapper'>
-            <img src='portfolio 1.jpg' alt='' />
-          </div>
-          <div className='imageWrapper'>
-            <img src='portfolio 1.jpg' alt='' />
-          </div>
-          <div className='imageWrapper'>
-            <img src='portfolio 1.jpg' alt='' />
-          </div>
-          <div className='imageWrapper'>
-            <img src='portfolio 1.jpg' alt='' />
-          </div>
-          <div className='imageWrapper'>
-            <img src='portfolio 1.jpg' alt='' />
-          </div>
+          <BsArrowLeftCircleFill
+            className='arrow arrow-left'
+            onClick={prevSlide}
+          />
+          {images.map((image, idx) => (
+            <div
+              className={
+                slide === idx ? 'imageWrapper' : 'imageWrapper slide-hidden'
+              }
+              key={image.id}
+            >
+              <img src={image.img} alt='' />
+            </div>
+          ))}
+          <BsArrowRightCircleFill
+            className='arrow arrow-right'
+            onClick={nextSlide}
+          />
+          <span className='indicators'>
+            {images.map((_, idx) => (
+              <button
+                key={idx}
+                onClick={() => setSlide(idx)}
+                className={
+                  slide === idx ? 'indicator' : 'indicator indicator-inactive'
+                }
+              ></button>
+            ))}
+          </span>
         </div>
       </div>
     </div>
