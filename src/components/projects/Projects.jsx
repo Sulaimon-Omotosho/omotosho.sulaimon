2faa6974f982f0ccab94b46e2679d0ff550ce5b0
Sulaimon Omotosho <susudef2u@gmail.com>
2024-03-05 09:43:13 +0100

    Working on projects slides

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -3,12 +3,43 @@ import './projects.scss'
 import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from 'react-icons/bs'
 
 const images = [
-  { id: '1', img: 'pro 1.jpg' },
+  {
+    id: '1',
+    img: 'pro 1.jpg',
+    title: 'Peppertea',
+    desc: "Dive into PepperTea's hair artistry showcased in a sleek portfolio site by me. Experience creativity and precision, reflecting the essence of PepperTea's unique expertise",
+    link: 'peppertea-portfolio.vercel.app',
+  },
   // { id: '2', img: 'pro 2.jpg' },
-  { id: '3', img: 'pro 3.jpg' },
-  { id: '4', img: 'pro 4.jpg' },
-  { id: '5', img: 'pro 5.jpg' },
-  { id: '6', img: 'pro 6.jpg' },
+  {
+    id: '3',
+    img: 'pro 3.jpg',
+    title: 'Layo Oguntimehin',
+    desc: "Explore Layo Oguntimehin's digital marketing prowess in a portfolio site crafted by me. Experience the essence of her dynamic strategies and expertise.",
+    link: 'layo.vercel.app',
+  },
+  {
+    id: '4',
+    img: 'pro 4.jpg',
+    title: 'BuyFood.ng',
+    desc: 'Indulge your culinary desires with BuyFood.ng, a web app artfully designed by me. Explore a seamless culinary journey as you navigate through the flavors and choices available at your fingertips.',
+    link: 'buy-food-ng.vercel.app',
+  },
+  {
+    id: '5',
+    img: 'pro 5.jpg',
+    title: 'Brich Africana',
+    desc: 'Discover career empowerment with Brich Africana. Navigate through their services on a website crafted by me, designed to elevate your professional journey.',
+
+    link: 'brich-africana.vercel.app',
+  },
+  {
+    id: '6',
+    img: 'pro 6.jpg',
+    title: 'Realtor.com',
+    desc: 'Explore the real estate landscape seamlessly with Realtor.com, a web app designed by me. Discover listings and navigate the property market effortlessly, all at your fingertips.',
+    link: 'realtor-site-v2.vercel.app',
+  },
 ]
 
 const Projects = () => {
@@ -45,6 +76,13 @@ const Projects = () => {
               key={image.id}
             >
               <img src={image.img} alt='' />
+              <div className='imgDesc'>
+                <h3>{image.title}</h3>
+                <p>{image.desc}</p>
+                <a href={image.link}>
+                  <button>Visit Site</button>
+                </a>
+              </div>
             </div>
           ))}
           <BsArrowRightCircleFill
